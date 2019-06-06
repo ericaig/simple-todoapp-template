8c5963c09bbcf4ab8875961e2380cbf2cfc90058
Eric Aig <deleaighewi@hotmail.com>
2019-06-06 21:50:09 +0200

    added list section

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('My todo template!');
+  // Bulma - Dropdowns
+
+  var $dropdowns = getAll('.dropdown:not(.is-hoverable)');
+
+  if ($dropdowns.length > 0) {
+    $dropdowns.forEach(function ($el) {
+      $el.addEventListener('click', function (event) {
+        event.stopPropagation();
+        $el.classList.toggle('is-active');
+      });
+    });
+
+    document.addEventListener('click', function (event) {
+      closeDropdowns();
+    });
+  }
+
+  function closeDropdowns() {
+    $dropdowns.forEach(function ($el) {
+      $el.classList.remove('is-active');
+    });
+  }
+
+  function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+  }
 });
